cd46ec21a0f91a2df8c1b5718068f4d56be8ed0c
hebrohim <raheemibrohim@mail.com>
2023-03-21 17:22:13 +0000

    :first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Data from "./data.json"
+const App = () => {
+
+  console.log(Data)
+  return (
+    <div className='bg-gray-200 h-screen'>
+      dd
+      </div>
+  )
+}
+
+export default App
+
+
