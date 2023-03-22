be224a4c87b817970e0e8547bcb8a124932ffe38
hebrohim <raheemibrohim@mail.com>
2023-03-22 15:03:22 +0000

    feat:Add filter function to filter country by region and/or continent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
 import Data from "./data.json";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import SelectOption from "./components/SelectOption";
 import Country from "./components/Country";
 const App = () => {
-  console.log(Data);
+  // console.log(Data);
+  const [Countries, setCountries] = useState(Data);
+
+  const filterCountry = (regionName) => {
+    let newRegions = Data.filter((Country) => Country.region == regionName);
+    setCountries(newRegions);
+  };
   return (
     <div className="bg-gray-200">
       <Header />
       <Input />
-      <SelectOption />
-      {
-        Data.map((country,index)=>{
-         return <Country country ={country} key ={index} />
-        })
-      }
+      <SelectOption filterCountry={filterCountry} />
+      {Countries.map((country, index) => {
+        return <Country country={country} key={index} />;
+      })}
     </div>
   );
 };
--- src/components/SelectOption.js
+++ src/components/SelectOption.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
+import Data from "../data.json";
 
-const SelectOption = () => {
+
+const SelectOption = ({ filterCountry }) => {
+  var Regions = ["Filter by Region",...new Set(Data.map((country) => country.region))];
+  //    console.log(Regions)
   return (
-    <div className=''>
-
-
-        <select className=' w-1/2 flex justify-between py-3 ml-9 pl-5 rounded-md text-sm'>
-            
-            <option>Filter by Region</option>
-            <option>Africa</option>
-            <option>America</option>
-            <option>Asia</option>
-            <option>Europe</option>
-            <option>Oceania</option>
-        </select>
+    <div className="">
+      <select
+        onChange={(e) => filterCountry(e.target.value)}
+        className=" w-1/2 flex justify-between py-3 ml-9 pl-5 rounded-md text-sm"
+      >
+        {Regions.map((region,index) => {
+          return (
+            <option value={region} id={region} key = {index}>
+              {region}
+            </option>
+          );
+        })}
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default SelectOption
+export default SelectOption;
