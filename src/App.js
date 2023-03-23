de1ec95372f1e737b75a2851ba78ab4470a5cccb
hebrohim <raheemibrohim@mail.com>
2023-03-23 16:48:08 +0000

    fix:allow user to be able to type country name in any case (convert to lowercase)

--- src/App.js
+++ src/App.js
@@ -20,37 +20,35 @@ const App = () => {
   return (
     <div className="bg-gray-200">
       <Header />
-<section className="md:flex items-center justify-between w-[80vw]">
-      <div className=" my-6 mx-9 shadow-xl">
-        <div className=" my-6  shadow-xl flex items-center w-full bg-white rounded-md pl-5">
-          <MdSearch className="text-gray-500 text-xl" />
+      <section className="md:flex items-center justify-between w-[80vw]">
+        <div className=" my-6 mx-9 shadow-xl">
+          <div className=" my-6  shadow-xl flex items-center w-full bg-white rounded-md pl-5 md:my-0">
+            <MdSearch className="text-gray-500 text-xl" />
 
-          <input
-            onChange={(event) => {
-              setQuery(event.target.value);
-            }}
-            placeholder="Search for a country..."
-            type="text"
-            name="search"
-            className=" placeholder:text-sm w-full  bg-white rounded-r-md border-none focus:border-white py-2 pl-9 pr-3 shadow-sm "
-          />
+            <input
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
+              placeholder="Search for a country..."
+              type="text"
+              name="search"
+              className=" placeholder:text-sm w-full  bg-white rounded-r-md border-none focus:border-white py-2 pl-9 pr-3 shadow-sm  "
+            />
+          </div>
         </div>
-      </div>
 
-      <SelectOption filterCountry={filterCountry} />
-
+        <SelectOption filterCountry={filterCountry} />
       </section>
 
       <section className="md:grid grid-cols-3">
-      {Countries.filter((country) =>
-        country.name.toLowerCase().includes(Query)
-      ).map((country, index) => {
-        return (
-          <Country country={country} key={index} displayFull={displayFull} />
-        );
-      })}
-
-</section>
+        {Countries.filter((country) =>
+          country.name.toLowerCase().includes(Query.toLowerCase())
+        ).map((country, index) => {
+          return (
+            <Country country={country} key={index} displayFull={displayFull}/>
+          );
+        })}
+      </section>
     </div>
   );
 };
