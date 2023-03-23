bba89fbb88051ea5e05052f4d70a615241a0551f
hebrohim <raheemibrohim@mail.com>
2023-03-23 08:23:11 +0000

    fix:fix 'search country'input

--- src/App.js
+++ src/App.js
@@ -4,29 +4,46 @@ import Header from "./components/Header";
 import Input from "./components/Input";
 import SelectOption from "./components/SelectOption";
 import Country from "./components/Country";
+import { MdSearch } from "react-icons/md";
 const App = () => {
   // console.log(Data);
   const [Countries, setCountries] = useState(Data);
-
+  const [Query, setQuery] = useState("");
   const filterCountry = (regionName) => {
     let newRegions = Data.filter((Country) => Country.region == regionName);
     setCountries(newRegions);
   };
 
-
-  const userInput= (e) =>{
-
-    let value  = e.target.value.toLowerCase() 
-    let newCountries = Countries.filter((country)=>country.name.toLowerCase().includes(value))
-setCountries(newCountries)
- }
+  const displayFull = (e) => {
+    console.log("display");
+  };
   return (
     <div className="bg-gray-200">
       <Header />
-      <Input Countries = {Countries} userInput = {userInput}/>
+
+      <div className=" my-6 mx-9 shadow-xl">
+        <div className=" my-6  shadow-xl flex items-center w-full bg-white rounded-md pl-5">
+          <MdSearch className="text-gray-500 text-xl" />
+
+          <input
+            onChange={(event) => {
+              setQuery(event.target.value);
+            }}
+            placeholder="Search for a country..."
+            type="text"
+            name="search"
+            className=" placeholder:text-sm w-full  bg-white rounded-r-md border-none focus:border-white py-2 pl-9 pr-3 shadow-sm "
+          />
+        </div>
+      </div>
+
       <SelectOption filterCountry={filterCountry} />
-      {Countries.map((country, index) => {
-        return <Country country={country} key={index} />;
+      {Countries.filter((country) =>
+        country.name.toLowerCase().includes(Query)
+      ).map((country, index) => {
+        return (
+          <Country country={country} key={index} displayFull={displayFull} />
+        );
       })}
     </div>
   );
